fe0e8535a9232c1016e0d352df115171c8503bd0
nhporti2 <nhporti2@gmail.com>
2021-04-16 20:52:16 -0500

    Create index.py

---
+++ index.py
@@ -0,0 +1,25 @@
+#Given a list as a parameter,write a function that returns a list of numbers that are less than ten
+#For example: Say your input parameter to the function is [1,11,14,5,8,9]...Your output should [1,5,8,9]
+
+def func(list):
+    
+    def list():
+        num = [num for num in list if num <10]
+    return nums
+list (1,11,14,5,8,9) 
+
+#Write a function that takes in two lists and returns the two lists merged together and sorted
+#Hint: You can use the .sort() method
+
+l_1 = [1,2,3]
+l_2 = [4,5,6,7]
+#Expected: [1,2,3,4,5,6,7]
+
+def join_lists(l_1, l_2):
+    
+    join_list - l_1 + l_2
+    join_lists.sort()
+    return join_lists
+
+join_lists(l_1, l_2)
+print(join_lists)
